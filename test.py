c085ed652c9cc3f2df4fc0942e4f260bce09e79e
Karthikeyan <58913393+karthikeyan0312@users.noreply.github.com>
2021-08-03 21:37:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-from textblob import TextBlob
 import streamlit as st
- 
+from textblob import TextBlob
+
 st.write("# Word Corrector")
 test = st.text_input("Enter the senteance :")
 text = TextBlob(test)
 # using TextBlob.correct() method
 text = text.correct()
-
 st.write("#",text)
